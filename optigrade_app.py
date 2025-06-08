48ff8f8784544b6a3c238780ff729f760fc4e238
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-06-08 17:33:51 +0100

    Update optigrade_app.py

--- optigrade_app.py
+++ optigrade_app.py
@@ -4,7 +4,6 @@ import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import google.generativeai as genai
-import requests
 import os
 from dotenv import load_dotenv
 
